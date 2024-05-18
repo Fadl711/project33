c19a9b6e513493f1f9a7775591c6817ce64dca74
FadlAlmatari <fathlvvee@gmail.com>
2024-05-18 18:59:15 +0300

    win

---
+++ resources/views/index.blade.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My websit</title>
+    <link rel="stylesheet" href="assets/css/leon.css">
+    <link rel="stylesheet" href="assets/css/normaliz.css">
+    <!-- Font Awesome Library -->
+    <link rel="stylesheet" href="assets/css/all.min.css">
+    <!-- The fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <!-- Start Header -->
+    <div class="header">
+        <div class="container">
+            <img class="logo" src="assets/images/logo.png" alt="">
+            <div class="links">
+                
+                <span class="icon">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </span>
+                <ul>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#portfolio">Portfolio</a></li>
+                    <li><a href="#aAbout">About</a></li>
+                    <li><a href="#content">Contact</a></li>
+                    <li><a href="/admin" >Log in </a></li>
+                    
+                </ul>
+
+            </div>
+        </div>
+    </div>
+    <!-- End Header -->
+    <!-- Start landing section -->
+    <div class="landing">
+        <div class="intro-text">
+            <h1>Hello There</h1>
+            <p>This is my websit , you can Learn hare Html and Css</p>
+        </div>
+    </div>
+    <!-- End landing section -->
+    <!-- Start features -->
+    <div class="features">
+        <div class="container">
+            <div class="feat">
+                <i class="fa-solid fa-wand-magic-sparkles fa-2xl"></i>
+                <h3>We will Do Ali The Work</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit , sed do eiusmod tempor incididunt ut lab</p>
+            </div>
+            <div class="feat">
+                <i class="fa-regular fa-gem fa-2xl"></i>
+                <h3>We will Do Ali The Work</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit , sed do eiusmod tempor incididunt ut lab</p>
+            </div>
+            <div class="feat">
+                <i class="fa-solid fa-earth-asia fa-2xl"></i>
+                <h3>We will Do Ali The Work</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit , sed do eiusmod tempor incididunt ut lab</p>
+            </div>
+        </div>
+    </div>
+    <!-- End features -->
+    <!-- Start Services -->
+    <div class="services" id="services">
+        <div class="container">
+            <h2 class="special-heading">Services</h2>
+            <p>Don't be bussy, be productive</p>
+            <div class="services-content">
+                <div class="col">
+                    <!-- Start -->
+                    <div class="srv">
+                        <i class="fa-solid fa-palette fa-2xl"></i>
+                        <div class="text">
+                            <h3>Graphic Design</h3>
+                            <p>kasafaf asfwwefsdf asfasf os tk afpsf pf asdpwf sassffpwsf ads aspddcwa sdsasd asdpdwasd pasd pr
+                                reasdsdasdsd, asdwasdwad asd asdwasdawasd.</p>
+                        </div>
+                    </div>
+                    <div class="srv">
+                        <i class="fa-brands fa-sketch fa-2xl"></i>
+                        <div class="text">
+                            <h3>Graphic Design</h3>
+                            <p>kasafaf asfwwefsdf asfasf os tk afpsf pf asdpwf sassffpwsf ads aspddcwa sdsasd asdpdwasd pasd pr
+                                reasdsdasdsd, asdwasdwad asd asdwasdawasd.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <!-- Start -->
+                    <div class="srv">
+                        <i class="fa-solid fa-vector-square fa-2xl"></i>
+                        <div class="text">
+                            <h3>Graphic Design</h3>
+                            <p>kasafaf asfwwefsdf asfasf os tk afpsf pf asdpwf sassffpwsf ads aspddcwa sdsasd asdpdwasd pasd pr
+                                reasdsdasdsd, asdwasdwad asd asdwasdawasd.</p>
+                        </div>
+                    </div>
+                    <div class="srv">
+                        <i class="fa-solid fa-pen-ruler fa-2xl"></i>
+                        <div class="text">
+                            <h3>Graphic Design</h3>
+                            <p>kasafaf asfwwefsdf asfasf os tk afpsf pf asdpwf sassffpwsf ads aspddcwa sdsasd asdpdwasd pasd pr
+                                reasdsdasdsd, asdwasdwad asd asdwasdawasd.</p>
+                        </div>
+                    </div>
+                    <!-- End -->
+                </div>
+                <div class="col">
+                    <div class="image image-column">
+                        <img src="assets/images/thumb-1920-874519.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Services --> 
+    <!--/* Start Portfolio */ -->
+    <div class="portfolio" id="portfolio">
+        <div class="container">
+            <h2 class="special-heading">Portfolio</h2>
+            <p>If you do it right, it will last forever.</p>
+            <div class="portfolio-content">
+                <div class="card">
+                    <img src="assets/images/OIP (1).jpg" alt="">
+                    <div class="info">
+                        <h3>Project Here</h3>
+                        <p>My creative ability is very difficult to measure because it can manifest in so many surprising and.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="assets/images/R.jpg" alt="">
+                    <div class="info">
+                        <h3>Project Here</h3>
+                        <p>My creative ability is very difficult to measure because it can manifest in so many surprising and.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="assets/images/5_041318044557.jpg" alt="">
+                    <div class="info">
+                        <h3>Project Here</h3>
+                        <p>My creative ability is very difficult to measure because it can manifest in so many surprising and.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- /* End Portfolio */ -->
+    <!--/* Start  About */ -->
+    <div class="about" id="aAbout">
+        <div class="container">
+            <h2 class="special-heading">About</h2>
+            <p>Less is more work.</p>
+            <div class="about-content">
+                <div class="image">
+                    <img src="assets/images/OIP.jpg" alt="">
+                </div>
+                <div class="text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quis harum obcaecati vitae iure necessitatibus nobis unde ratione illum nemo placeat autem atque, modi ad, exercitationem deleniti quisquam sapiente totam!</p>
+                    <hr>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, fuga nesciunt! Quisquam fugit quam illum consequuntur possimus, labore officia id aperiam dolorum, reprehenderit excepturi dolor esse inventore ad, harum soluta.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- /* End About */ -->
+    <!-- Start content -->
+    <div class="content" id="content">
+        <div class="container">
+            <h2 class="special-heading">content</h2>
+            <p>We are born to create</p>
+            <div class="info">
+                <p class="label">Feel free to drop us a line at :</p>
+                <a href="mailto:fathlvvee@gmail.com?subject=Content" class="link">fathlvvee@gmail.com</a>
+                <div class="social">
+                    Find us on Social Networks
+                    <a href ="https://www.facebook.com/fathlvvee" target="blank"><i class="fa-brands fa-facebook"></i></a>
+                    <a href ="https://twitter.com/cNPA2orqo2OOkiA" target="blank" ><i class="fa-brands fa-twitter"></i></a>    
+                    <a href ="https://www.instagram.com/?utm_source=pwa_homescreen&__pwa=1" target="blank" ><i class="fa-brands fa-instagram"></i></a>    
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End content -->
+    <!-- Start Footer -->
+    <div class="footer">
+        &copy; 2024 <span>Fadl Al-Matari</span> All Reserved
+    </div>
+
+    <!-- End Footer -->
+        
+</body>
+</html>
